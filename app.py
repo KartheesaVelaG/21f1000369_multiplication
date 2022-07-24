3f07fc134090835cf6d4fa83069ad9447f930971
KartheesaVelaG <109903856+KartheesaVelaG@users.noreply.github.com>
2022-07-24 22:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 st.write('Multiplication of 2 given numbers.')
